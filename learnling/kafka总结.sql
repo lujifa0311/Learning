88aab1eb77bf6a33bbc9db78a4f552b1f31e72ed
jifa.lu <lujifa@163.com>
2020-11-23 17:10:58 +0800

    新增kafka命令

--- learnling/kafka总结.sql
+++ learnling/kafka总结.sql
@@ -3,3 +3,4 @@ bin/kafka-topics.sh --zookeeper  localhost:2181 --describe --topic customer-stat
 watch -n 5 -d './kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group customer-stat-update-consumer-group_su1 | grep customer-stat-update'
 
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group  customer-stat-update-consumer-group_su1 --topic customer-stat-update
+sdafa
